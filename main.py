e6bad84dd1f512c843b86e8f24e3236868da67ed
Emma Bates <100385290+ebates1977@users.noreply.github.com>
2022-03-04 02:28:08 +0000

    finished question 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+import math
+
+# 1.1
+
+n = 42
+
+# 1.2
+tau = (math.pi) * 2
+tau = round(tau, 4)
+
+# 1.3
+school = "Sault College"
+print(str(n) + ", " + str(tau) + ", " + school)
+
+
+

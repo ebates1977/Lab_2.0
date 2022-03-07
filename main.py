245dd3d6b5a68fef65262db4ff6cfa8b6467ca84
Emma Bates <100385290+ebates1977@users.noreply.github.com>
2022-03-07 00:15:23 +0000

    formatted and reviewed

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 import math
+def part1():
+ 
+  n = 42
+  tau = (math.pi) * 2
+  tau = round(tau, 4)
+  school = "Sault College"
+  print(str(n) + ", " + str(tau) + ", " + school)
 
-# 1.1
+part1()
 
-n = 42
+def part2():
+  college_class = "CSD110"   #class is a keyword in python
+  desired_grade = 100        #can't use a dash in a variable name
+  two_hot = "hothot"  #cant start a variable with an integer
+  pi = 3.14159  #wrong order
+  print(college_class + ", " + str(desired_grade) + ", " + two_hot  + ", " + str(pi))
 
-# 1.2
-tau = (math.pi) * 2
-tau = round(tau, 4)
+part2()
 
-# 1.3
-school = "Sault College"
-print(str(n) + ", " + str(tau) + ", " + school)
+def part3():
+  repeat_hot = "Hot" * 3
+  print("Feeling..." + repeat_hot + "!")
 
-# 2
-college_class = "CSD110"   #class is a keyword in python
-desired_grade = 100        #can't use a dash in a variable name
-two_hot = "hothot"  #cant start a variable with an integer
-pi = 3.14159  #wrong order
-
-print(college_class + ", " + str(desired_grade) + ", " + two_hot  + ", " + str(pi))
-
-# 3
-repeat_hot = "Hot" * 3
-print("Feeling..." + repeat_hot + "!")
-
-def main():
+part3()
+  
+def part4():
   base = 3000    # in cm
   height = 1000  # in cm
-  volume = 1/3 * (base * base) * height
+  volume = 1/3 * pow(base, 2) * height
   print("The volume of a square pyramid with base " + str(base) + "cm and height " + str(height) + "cm is " + str(volume) + "cm^3")
 
 # Ignore this for now; you will learn what this is later
+
 if __name__ == "__main__":
-    main()
+    part4()
+
